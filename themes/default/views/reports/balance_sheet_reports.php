de69b26042196441cafdc15956cb12d84072c498
CloudMFI <cloudmfi168@gmail.com>
2017-11-30 08:59:35 +0630

    amm:change balance sheet

--- themes/default/views/reports/balance_sheet_reports.php
+++ themes/default/views/reports/balance_sheet_reports.php
@@ -1,3 +1,4 @@
+<!-- Old Version
 <link href="https://fonts.googleapis.com/css?family=Moul|Battambang" rel="stylesheet"> 
 <style type="text/css">
 	.a_four_page,.header_report{
@@ -436,4 +437,592 @@
 		</table>
 		<br/><br/>
 	</div>
+</div> 
+Old Version-->
+
+
+
+
+
+
+
+
+
+
+<link href="https://fonts.googleapis.com/css?family=Moul|Battambang" rel="stylesheet"> 
+<style type="text/css">
+	.a_four_page,.header_report{
+		width:21cm;
+		min-height:height:29.7cm;
+		height:auto;
+		margin:0 auto;
+		font-size:13px;
+		font-family:'Zawgyi-One' Time New Roman';
+	}
+	.header_report{
+		height:100px;
+		//border:1px solid red;
+	}
+	.title{
+		text-align: center;
+		font-size:16px;
+		font-family: 'Zawgyi-One', cursive;
+	}
+	#logo{
+		width: 150px;
+		opacity: 0.8;
+	}
+	.tableone thead{
+		width: 100%;
+		font-family: 'Zawgyi-One','Time New Roman';
+		font-size: 12px;
+		background:#92CDDC;
+		font-weight:bold;
+
+	}
+	table tr td{
+		border:1px solid black;
+		
+	}
+	
+
+	
+</style>
+
+<div class="body-overflow">
+	<div class="a_four_page" >
+		<div class="header_report">
+			<div class="title">
+				<span id="logo">
+					<?php if ($Settings->logo2) {echo '<img src="' . site_url() . 'assets/uploads/logos/' . $Settings->logo2 . '" />';} ?><br><br><br>
+				</span>
+				လက္က်န္ရွင္းတမ္း<br>
+				Balance Sheet
+			</div>
+			<!-- <h4><strong>កាលបរិច្ឆេទ៖</strong></h4> -->
+			<!-- <h4><strong>ឈ្មោះគ្រឹះស្ថាន   ៖</strong></h4> -->
+		</div>
+		
+		<table style="font-family:'Zawgyi-One'";>
+		  <tr>
+			<td style="border:none; text-align:right; width:60%; ">
+				ေငြပမာဏ (က်ပ္သန္းေပါင္း )<br>
+				Amounts in millions of Kyats 
+			</td>
+			<td style="border:none; width:10%; "></td>
+			<td style="border:none; width:15%; "></td>
+			<td style="border:none; width:10%; "></td>
+		  </tr>
+		  <tr>
+		  	<td style="width: 55%;border:none;"></td>
+		  	<td style="width: 45%;border:none; ">
+		  		ရက္စဲြ (:dd-mmm-yyyy: )  ..……………………………………
+		  	</td>
+		  </tr>
+		  <tr>
+		  	<td style="width: 55%;border:none;"></td>
+		  	<td style="width: 45%;border:none; ">
+		  		စာရင္းေပးပို ့သည့္ကာလ (month )………………………………
+		  	</td>
+		  </tr>
+		</table><br>
+	</div>
 </div>
+		
+		<table class="tableone">
+		  <thead>
+		  	<tr>
+			  	<td style="text-align:center;width: 3%;padding: 20px">စဥ္<br>No</td>
+				<td style="text-align:center;font-family: 'Zawgyi-One'; width:25%;">စာရင္းအမည္မ်ား</td>
+				<td width="18%" style="text-align:center;">က်ပ္<br>Kyats</td>
+				<td width="18%" style="text-align:center;">အျခားနိုင္ငံျခားေငြမ်ားကို က်ပ္ေျပာင္းလဲ<br/>Other foreign 	currencies into Kyats
+				</td>
+				<td width="18%" style="text-align:center;">စုစုေပါင္းက်ပ္<br>Total kyats</td>
+				<td width="18%" style="text-align:center;">စတင္နွစ္မွ ယေန့ထိက်ပ္<br>Year to Date in Kyats</td>
+		  	</tr>
+		  </thead>
+		  <tbody>
+		  	<tr style="background:#DAEEF3;border-style: none;">
+		  		<td colspan="6" style="padding: 5px;font-weight: bold;text-align: center;">ရပိုင္ခြင့္မ်ား<br>Assests</td>
+		  	</tr>
+		  	<tr>
+		  		<td>၁<br>1</td>
+		  		<td>
+		  			ေငြသားနွင့္ဘဏ္မ်ားရွိလက္က်န္စာရင္းမ်ား<br>Cash and Balances in Banks
+		  		</td>
+		  		<td>1,000,000,000</td>
+		  		<td>1,000,000,000</td>
+		  		<td>1,000,000,000</td>
+		  		<td>1,000,000,000</td>
+		  	</tr>
+		  	<tr>
+		  		<td>၁.၁<br>1.1</td>
+		  		<td>
+		  			လက္ထဲရွိေငြသားနွင့္ မီးခံေသတၱာထဲရွိေငြ<br>Cash on Hand and in Vault
+		  		</td>
+		  		<td></td>
+		  		<td></td>
+		  		<td></td>
+		  		<td></td>
+			</tr>
+			<tr>
+				<td>၁.၂<br>1.2</td>
+				<td>
+					ဘဏ္မ်ားနွင့္ အျခားေငြေၾကး အဖဲြ့အစည္းမ်ားရွိ လက္က်န္ေငြ<br>Balances with Banks and Other Finicial Institutions
+				</td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>
+			<tr>
+				<td>၂<br>2</td>
+				<td>ေစ်းကြက္တင္ေရာင္းခ်နိုင္ေသာစာခ်ဳပ္မ်ားနွင့္ အခ်ိန္တို ရင္းႏွီးျမွဳပ္ႏွံမႈမ်ား<br>Marketable Securities & Short-Term Investments
+				</td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>
+		  </tbody>
+		</table>
+		  
+		  <!-- <tr style="background:#DAEEF3">
+			<td style="text-align:left; font-family: 'Zawgyi-One'; font-size:12px;">
+				១. សាច់ប្រាក់ និងសមតុល្យនៅធនាគារជាតិនៃកម្ពុជានិងនៅធនាគារផ្សេងៗ
+			</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  <tr>
+			<td style="text-align:left; padding-left:50px;">១.១. សាច់ប្រាក់ក្នុងដៃ (ជារៀលនិងរូបិយប៍ណ្ណបរទេស)</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  <tr>
+			<td style="text-align:left; padding-left:50px;">១.២. ប្រាក់បញ្ញើនៅធនាគារជាតិនៃកម្ពុជា   </td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  <tr>
+			<td style="text-align:left; padding-left:50px;">១.៣. ប្រាក់បញ្ញើនៅធនាគារផ្សេងៗ</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  <tr>
+			<td style="text-align:left; padding-left:50px;">១.៤. ប្រាក់បញ្ញើផ្សេងៗ</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  <tr style="background:#DAEEF3">
+			<td style="text-align:left; font-family: 'Zawgyi-One';font-size:12px;">២. ឥណទាននិងបុរេប្រទានចំពោះអតិថិជន</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  <tr>
+			<td style="text-align:left; padding-left:50px;">២.១. សមតុល្យឥណទានសរុប</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  <tr>
+			<td style="text-align:left; padding-left:100px;">២.១.១. ដែលក្នុងនោះសមតុល្យនៅសល់តិចជាងមួយខែ</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  <tr>
+			<td style="text-align:left; padding-left:100px;">២.១.២. ដក៖ ប្រាក់បម្រុងសម្រាប់ការបាត់បង់</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  <tr>
+			<td style="text-align:left; padding-left:50px;">២.២. សមតុល្យឥណទានសុទ្ធ</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  <tr>
+			<td style="text-align:left; padding-left:50px;">២.៣. ការប្រាក់បង្គរត្រូវទទួល</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  
+		  <tr style="background:#DAEEF3">
+			<td style="text-align:left; font-family: 'Zawgyi-One';font-size:12px;">៣. អចលនទ្រព្យ</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  <tr>
+			<td style="text-align:left; padding-left:50px;">៣.១. ដី (សុទ្ធ)</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  <tr>
+			<td style="text-align:left; padding-left:100px;">៣.១.១. ដី (តម្លៃសរុប)</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  <tr>
+			<td style="text-align:left; padding-left:100px;">៣.១.២. ដក៖ រំលស់បង្គរ</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  <tr>
+			<td style="text-align:left; padding-left:50px;">៣.២. អគារ (សុទ្ធ)</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  <tr>
+			<td style="text-align:left; padding-left:50px;">៣.២.១. អគារ (តម្លៃសរុប)</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  <tr>
+			<td style="text-align:left; padding-left:50px;">៣.២.២. ដក៖ រំលស់បង្គរ</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  <tr>
+			<td style="text-align:left; padding-left:50px;">៣.៣. អចលនទ្រព្យផ្សេងៗ (សុទ្ធ)</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  <tr>
+			<td style="text-align:left; padding-left:100px;">៦.៣.១. អចលនទ្រព្យផ្សេងៗ (តម្លៃសរុប)</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  <tr>
+			<td style="text-align:left; padding-left:100px;">៦.៣.២. ដក៖ រំលស់បង្គរ</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  <tr>
+			<td style="text-align:left; padding-left:50px;">៣.៤. ទ្រព្យសកម្មអរូបិយ (សុទ្ធ)</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  <tr>
+			<td style="text-align:left; padding-left:100px;">៦.៤.១. ទ្រព្យសកម្មអរូបិយ (តម្លៃសរុប)</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  <tr>
+			<td style="text-align:left; padding-left:100px;">៦.៤.២. ដក៖ រំលស់បង្គរ</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  
+		  <tr style="background:#DAEEF3">
+			<td style="text-align:left; font-family: 'Zawgyi-One';font-size:12px;">៤. ទ្រព្យសកម្មដែលត្រូវទទួល និងទ្រព្យសកម្មផ្សេងៗ</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  <tr>
+			<td style="text-align:left; padding-left:50px;">៤.១. ជំពូកសម្រាប់ធ្វើបេឡានិច្ច័យ</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  <tr>
+			<td style="text-align:left; padding-left:50px;">៤.២. ទ្រព្យសកម្មផ្សេងៗ</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  <tr style="background:#92CDDC">
+			<td style="text-align:left; font-family: 'Zawgyi-One';font-size:12px;">សរុបទ្រព្យសកម្ម</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  <tr style="background:#92CDDC;font-family: 'Zawgyi-One';font-weight:bold">
+			<td rowspan="2" style="text-align:left; font-family: 'Zawgyi-One';font-size:12px;">ទ្រព្យអកម្ម និងមូលនិធិរបស់ភាគទុនិក</td>
+			<td>ជារៀល</td>
+			<td>រូបិយបណ្ណផ្សេងៗ <br/>គិតជារៀល</td>
+			<td>សរុបជារៀល</td>
+		  </tr>
+		  <tr style="background:#92CDDC;font-weight:bold">
+			<td>1</td>
+			<td>2</td>
+			<td>3 = 1+2</td>
+		  </tr>
+		  
+		  <tr>
+			<td style="text-align:left; font-family: 'Zawgyi-One';font-size:12px;">៥. ប្រាក់ជំពាក់ធនាគារជាតិនៃកម្ពុជានិងធនាគារផ្សេងៗ</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  <tr>
+			<td style="text-align:left; padding-left:50px;">៥.១. ប្រាក់ជំពាក់ធនាគារជាតិនៃកម្ពុជា</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  <tr>
+			<td style="text-align:left; padding-left:50px;">៥.២. ប្រាក់ជំពាក់ធនាគារផ្សេងៗ</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  <tr>
+			<td style="text-align:left; padding-left:50px;">៨.២. ការប្រាក់បង្គរត្រូវសង</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  <tr style="background:#DAEEF3">
+			<td style="text-align:left; font-family: 'Zawgyi-One';font-size:12px;">៦. ប្រាក់បញ្ញើរបស់អតិថិជន</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  <tr>
+			<td style="text-align:left; padding-left:50px;">៦.១. ប្រាក់សន្យំកាតព្វកិច្ច</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  
+		  <tr>
+			<td style="text-align:left; padding-left:50px;">៦.២. ប្រាក់សន្សំដោយស្ម័គ្រចិត្ត</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  <tr>
+			<td style="text-align:left; padding-left:100px;">៦.២.១. ប្រាក់បញ្ញើសន្សំសំចៃ</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  <tr>
+			<td style="text-align:left; padding-left:100px;">៦.២.២. ប្រាក់បញ្ញើចរន្ត</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  <tr>
+			<td style="text-align:left; padding-left:100px;">៦.២.៣. ប្រាក់បញ្ញើមានកាលកំណត់</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  <tr>
+			<td style="text-align:left; padding-left:50px;">៦.៣. ការប្រាក់បង្គរត្រូវសង</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  <tr style="background:#DAEEF3">
+			<td style="text-align:left; font-family: 'Zawgyi-One';font-size:12px;">៧. គណនីត្រូវសង និងទ្រព្យអកម្មផ្សេងៗ</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  <tr>
+			<td style="text-align:left; padding-left:50px;">៧.១.គណនីត្រូវសង</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  <tr>
+			<td style="text-align:left; padding-left:50px;">៧.២. បង្គរ</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  <tr>
+			<td style="text-align:left; padding-left:50px;">៧.៣. ពន្ធត្រូវបង់</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  <tr>
+			<td style="text-align:left; padding-left:50px;">៧.៤. ទ្រព្យអកម្មផ្សេងៗ</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  <tr>
+			<td style="text-align:left; font-family: 'Zawgyi-One';font-size:12px;">សរុបទ្រព្យអកម្ម</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  <tr style="background:#DAEEF3">
+			<td style="text-align:left; font-family: 'Zawgyi-One';font-size:12px;">៨. មូលនិធិរបស់ភាគទុនិក</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  <tr>
+			<td style="text-align:left; padding-left:50px;">៨.១. ដើមទុនលក្ខន្តិកៈ</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  <tr>
+			<td style="text-align:left; padding-left:100px;">៨.១.១. បុព្វលាភលើដើមទុនជាភាគហ៊ុន</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  <tr>
+			<td style="text-align:left; padding-left:100px;">៨.១.២. ដកចេញ៖ មិនទាន់បានបង់</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  <tr>
+			<td style="text-align:left; padding-left:50px;">៨.២. បំណុលបន្ទាប់បន្សំ (បានទទួលស្គាល់ពីធនាគារជាតិនៃកម្ពុជា)</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  <tr>
+			<td style="text-align:left; padding-left:50px;">៨.៣. ទុនបំរុងនិងសំវិធានធន</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  <tr>
+			<td style="text-align:left; padding-left:50px;">៨.៤. ចំណេញរក្សាទុក/ខាតពីឆ្នាំមុន</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  <tr>
+			<td style="text-align:left; padding-left:50px;">៨.៥. ចំណេញ/ខាតសុទ្ធក្នុងគ្រា</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  <tr>
+			<td style="text-align:left; padding-left:50px;">៨.៦. ហ៊ុនផ្សេងៗទៀតរបស់ភាគទុនិក</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  <tr>
+			<td style="text-align:left; padding-left:100px;">៨.៦.១. អំណោយ</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  <tr>
+			<td style="text-align:left; padding-left:100px;">៨.៦.២. មូលនិធិសាធារណៈ</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		  <tr style="background:#92CDDC">
+			<td style="text-align:left; font-family: 'Zawgyi-One';font-size:12px;">សរុបទ្រព្យអកម្ម និងមូលនិធិរបស់ភាគទុនិក</td>
+			<td>0</td>
+			<td style="text-align:right;">0</td>
+			<td style="text-align:right;">0</td>
+		  </tr>
+		</table><br/>
+	 -->	
+		<table >
+		<tr>
+			<td style="border:none; width:30%;"></td>
+			<td style="border:none; width:30%;"></td>
+			<td style="border:none; width:40%;">Phnom Penh, February 1, 2016</td>
+		  </tr>
+		  <tr>
+			<td style="border:none;">General Manager/Manger</td>
+			<td style="border:none;">Verified by</td>
+			<td style="border:none;">Prepared by</td>
+		  </tr>
+		</table>
+		<br/><br/>
+	
