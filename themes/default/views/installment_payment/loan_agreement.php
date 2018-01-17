05f07cd94b5bf4ad1f97264842da08aca1b45f52
CloudMFI <cloudmfi168@gmail.com>
2018-01-17 11:34:26 +0630

    amm:get new

--- themes/default/views/installment_payment/loan_agreement.php
+++ themes/default/views/installment_payment/loan_agreement.php
@@ -208,10 +208,45 @@
 					<p>ေငြေခ်းသူႏွင့္ ပူးတြဲ ေငြေခ်းသူ၏ ဘယ္လက္မလက္ေဗြ <br> ေငြေခ်းသူ	</p>
 				</div>				
 			</div>
+
 			
-			<div style=" width:100%;min-height:100px; padding-top:15px; padding-left:10px;">				 
+			
+				<?php 
+					$i = 1;
+					foreach($group_loan as $group){?>
+					<div>
+						<div style=" width:100%;min-height:50px; padding-top:15px; margin-left:20px;">
+							<?php echo $i?> <?= $group->customer_name?>
+						</div>
+						<div style="width: 100px;height: 110px;border:2px solid black;margin-left:20px; ">
+						</div><br>
+						<div class="row" style="margin-left:20px;"><label>Amount:</label><?= $this->erp->formatMoney($this->erp->convertCurrency($sale_item->currency_code, $setting->default_currency, $group->total)) ; ?>
+						</div>
+				</div>
+				<?php $i+=1;} ?>
+
+					
+						
+						
+				
+			
+			<!-- <div style=" width:100%;min-height:100px; padding-top:15px; padding-left:10px;">				 
 				1. ..................... &nbsp;&nbsp;&nbsp; 2. ...................... &nbsp;&nbsp;&nbsp; 3. ...................... &nbsp;&nbsp;&nbsp; 4. ...................... &nbsp;&nbsp;&nbsp; 5. ...................... &nbsp;&nbsp;&nbsp; 6. ......................
-			</div>		 
+			</div>
+			<div style=" width:100%;min-height:150px; padding-left:10px;">				 
+				<div style="width: 100px;height: 110px;border:2px solid black;float: left;margin-right: 28px;">
+				</div> 
+				<div style="width: 100px;height: 110px;border:2px solid black;float: left;margin-right: 28px;">
+				</div>
+				<div style="width: 100px;height: 110px;border:2px solid black;float: left;margin-right: 30px;">
+				</div>
+				<div style="width: 100px;height: 110px;border:2px solid black;float: left;margin-right: 32px;">
+				</div>  
+				<div style="width: 100px;height: 110px;border:2px solid black;float: left;margin-right: 33px;">
+				</div>
+				<div style="width: 100px;height: 110px;border:2px solid black;float: left;margin-right: 30px;">
+				</div>
+			</div> -->		 
 			
 <!--<?php echo($contract_info->gender=male?'checked="checked"':''); ?>
 	 <?php echo $contract_info->gender($gender=='female')?'checked':'' ?>
